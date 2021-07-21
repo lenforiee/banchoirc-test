166096c7a021626bdb2b4e42ba6e391a40e4fea0
lenforiee <lenforiee@gmail.com>
2021-07-21 17:52:08 +0200

    some more stuff

--- irc_test.py
+++ irc_test.py
@@ -1,16 +1,7 @@
 import socket
-import socketserver
-from typing import Set, Union
-
-# Custom Bancho IRC exception.
-class BanchoIRCException(Exception):
-    pass
-
-class BanchoIRC:
-    pass
-
-class BanchoClient:
-    pass
+import time
+import select
+from typing import Set, Union, List
 
 def safe_name(string: Union[str, bytes]):
     if isinstance(string, bytes):
@@ -18,11 +9,29 @@ def safe_name(string: Union[str, bytes]):
 
     return string.lower().replace(" ", "_").rstrip()
 
+# Custom Bancho IRC exception.
+class BanchoIRCException(Exception):
+    pass
+
+class BanchoIRC:
+    def __init__(self, name: str, address: tuple):
+        self.server_name: str = name
+        self.address: tuple = address
+        self.channels: List[BanchoChannel] = []
+        self.clients: Set[BanchoClient] = set()
+
+    def on_add_client(self, client: BanchoClient):
+        self.clients.add(client)
+
+    def on_remove_client(self, client: BanchoClient):
+        self.clients.remove(client)
+
 class BanchoChannel:
     """Represetnts a one bancho text channel"""
-    def __init__(self, server: BanchoIRC, name: bytes, desc: bytes, destruct: bool = True) -> None:
+    def __init__(self, server: BanchoIRC, name: bytes, desc: bytes, destruct: bool = True, autojoin: bool = False) -> None:
         self.server: BanchoIRC = server
         self.name: bytes = name
+        self.auto_join: bool = autojoin
         self.destructable: bool = destruct
         self.safe_name: bytes = safe_name(self.name)
         self.description: bytes = desc
@@ -36,3 +45,18 @@ class BanchoChannel:
         self.users.remove(user)
         if not self.users and self.destructable:
             self.server.destruct_channel(self)
+
+class BanchoClient:
+    def __init__(self, server: BanchoIRC, client: socket.socket):
+        self.server: BanchoIRC = server
+        self.client: socket.socket = client
+        self.nickname: bytes = None
+        self.ping_time: int = int(time.time())
+        self.queue: bytearray = bytearray() # Bytearray is much faster than bytes.
+        self.safe_nick: bytes = safe_name(self.nickname)
+        self.channels: List[BanchoChannel] = []
+    
+    def join_channel(self, channel: BanchoChannel):
+        if channel and channel not in self.channels:
+            self.channels.append(channel)
+            channel.on_user_join(self)
